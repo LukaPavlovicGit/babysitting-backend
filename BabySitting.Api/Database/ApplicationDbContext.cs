cfe17e9960500a0d3b43432c4e2061fe9211ef95
LukaPavlovic <lukapavlovic032@gmail.com>
2024-11-14 18:24:13 +0100

    dummy commit

--- BabySitting.Api/Database/ApplicationDbContext.cs
+++ BabySitting.Api/Database/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using BabySitting.Api.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
 
 namespace BabySitting.Api.Database;
 
@@ -14,7 +15,9 @@ public class ApplicationDbContext : IdentityDbContext<User>
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
-         
+
+        System.Diagnostics.Debug.WriteLine("");
+
 
         builder.HasDefaultSchema("identity");
     }
