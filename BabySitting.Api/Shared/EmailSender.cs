fdb450bf896ec5376a6488e5999c9eca5ee10693
LukaPavlovic <lukapavlovic032@gmail.com>
2024-11-20 14:05:44 +0100

    enabled sending confirmation emails

--- BabySitting.Api/Shared/EmailSender.cs
+++ BabySitting.Api/Shared/EmailSender.cs
@@ -1,18 +1,25 @@
 ﻿using System.Net.Mail;
 using System.Net;
+using BabySitting.Api.Entities;
+using Microsoft.AspNetCore.Identity;
+using System.Text.Encodings.Web;
 
 namespace BabySitting.Api.Shared;
 
 public interface ISenderEmail
 {
     Task SendEmailAsync(string ToEmail, string Subject, string Body, bool IsBodyHtml = false);
+    Task SendConfirmationEmail(string email, User user);
 }
 public class EmailSender : ISenderEmail
 {
     private readonly IConfiguration _configuration;
-    public EmailSender(IConfiguration configuration)
+    private readonly UserManager<User> _userManager;
+
+    public EmailSender(IConfiguration configuration, UserManager<User> userManager) 
     {
         _configuration = configuration;
+        _userManager = userManager;
     }
     public Task SendEmailAsync(string ToEmail, string Subject, string Body, bool IsBodyHtml = false)
     {
@@ -31,4 +38,14 @@ public class EmailSender : ISenderEmail
         };
         return client.SendMailAsync(mailMessage);
     }
+
+    public async Task SendConfirmationEmail(string email, User user)
+    {
+        var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+        string baseUrl = _configuration["AppBaseUrl"]!;
+        string confirmationLink = $"{baseUrl}/Account/ConfirmEmail?UserId={user.Id}&Token={token}";
+        string encodedLink = HtmlEncoder.Default.Encode(confirmationLink);
+        await SendEmailAsync(email, "Confirm Your Email", $"Please confirm your account by <a href='{encodedLink}'>clicking here</a>.", true);
+    }
+
 }
