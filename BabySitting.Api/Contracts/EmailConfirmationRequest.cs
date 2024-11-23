4ef0e5c3e2e32072f1035ec8bf22c332e608d60f
LukaPavlovic <lukapavlovic032@gmail.com>
2024-11-23 23:32:02 +0100

    removed EmailConfirmationRequest

---
+++ BabySitting.Api/Contracts/EmailConfirmationRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace BabySitting.Api.Contracts;
+public class EmailConfirmationRequest
+{
+    public string UserId { get; set; } = string.Empty;
+    public string Token { get; set; } = string.Empty;
+}
