c9c7bacde639bdb1ac45a9cd4adba76c7b46013c
LukaPavlovicGit <lukapavlovic032@gmail.com>
2024-11-25 01:54:27 +0100

    created AccountCompletionRequest class

---
+++ BabySitting.Api/Contracts/requests/AccountCompletionRequest.cs
@@ -0,0 +1,16 @@
+﻿using BabySitting.Api.Domain.Entities;
+using BabySitting.Api.Domain.Enums;
+
+namespace BabySitting.Api.Contracts.requests;
+public class AccountCompletionRequest
+{
+    public RoleEnum Role { get; set; } = RoleEnum.NONE;
+
+    public required FamilyInformation FamilyInformation { get; set; }
+
+    public required Job JobOffer { get; set; }
+
+    public String PhotoUrl { get; set; } = string.Empty;
+
+    public Boolean SubscribeToJobNotifications { get; set; } = false;
+}
