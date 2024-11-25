3f4467af43b5624a09967527ab460fca8f521d81
LukaPavlovicGit <lukapavlovic032@gmail.com>
2024-11-26 00:58:30 +0100

    AccountCompletionRequest changed

--- BabySitting.Api/Contracts/requests/AccountCompletionRequest.cs
+++ BabySitting.Api/Contracts/requests/AccountCompletionRequest.cs
@@ -2,15 +2,51 @@
 using BabySitting.Api.Domain.Enums;
 
 namespace BabySitting.Api.Contracts.requests;
+
 public class AccountCompletionRequest
 {
-    public RoleEnum Role { get; set; } = RoleEnum.NONE;
+    public RoleEnum Role { get; set; }
 
-    public required FamilyInformation FamilyInformation { get; set; }
+    public string PhotoUrl { get; set; } = string.Empty;
 
-    public required Job JobOffer { get; set; }
+    public bool SubscribeToJobNotifications { get; set; } = false;
 
-    public String PhotoUrl { get; set; } = string.Empty;
+    public OfferCreateDto? OfferCreateDto { get; set; }
 
-    public Boolean SubscribeToJobNotifications { get; set; } = false;
+    public FamilyCreateDto? FamilyInformationDto { get; set; }
+
 }
+
+public class FamilyCreateDto
+{
+    public Guid UserId { get; set; }
+
+    public string FirstName { get; set; } = string.Empty;
+    
+    public required string Address { get; set; }
+
+    public required HashSet<LanguagesEnum> SpeakingLanguages { get; set; }
+
+    public required int NumberOfChildren { get; set; }
+
+    public required HashSet<ChildAgeCategoryEnum> ChildrenAgeCategories { get; set; }
+
+    public required string FamilyDescription { get; set; }
+
+}
+
+public class OfferCreateDto
+{
+    public Guid UserId { get; set; }
+
+    public HashSet<SkillsEnum> PreferebleSkills { get; set; } = new HashSet<SkillsEnum>();
+
+    public CurrencyEnum Currency { get; set; }
+
+    public int Rate { get; set; }
+
+    public JobLocationEnum JobLocation { get; set; }
+
+    public required Schedule Schedule { get; set; }
+}
+
