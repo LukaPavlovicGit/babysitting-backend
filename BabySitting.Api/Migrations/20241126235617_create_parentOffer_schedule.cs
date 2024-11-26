dadc08d6b52bc214f2c50689d3fc1f1693b6f153
LukaPavlovicGit <lukapavlovic032@gmail.com>
2024-11-27 00:59:29 +0100

    added migrations

---
+++ BabySitting.Api/Migrations/20241126235617_create_parentOffer_schedule.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BabySitting.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class create_parentOffer_schedule : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetRoles_Name",
+                schema: "identity",
+                table: "AspNetRoles",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetRoles_Name",
+                schema: "identity",
+                table: "AspNetRoles");
+        }
+    }
+}
