e58c029dbffc0e5d20730c3953d2ab48970b9c7a
LukaPavlovicGit <lukapavlovic032@gmail.com>
2024-12-01 15:18:33 +0100

    created verifications table

---
+++ BabySitting.Api/Domain/Entities/Verifications.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BabySitting.Api.Domain.Entities;
+
+[Table("Verifications")]
+public class Verifications
+{
+    [Key]
+    public int Id { get; set; }
+
+    public Guid UserId { get; set; } 
+
+    public bool GovernmentIdProvided { get; set; } = false;
+
+    public bool EmailVerified { get; set; } = false;
+    
+    public bool PhonePhoneVerified { get; set; } = false;
+
+    public bool GoogleAccountVerified { get; set; } = false;
+
+    public bool FacebookAccountVerified {  get; set; } = false;
+    
+    public bool LinkedInAccountVerified { get; set; } = false;
+
+
+    public DateTime GovernmentIdProvidedAt { get; set; }
+
+    public DateTime EmailVerifiedAt { get; set; }
+
+    public DateTime PhonePhoneVerifiedAt { get; set; }
+
+    public DateTime GoogleAccountVerifiedAt { get; set; }
+
+    public DateTime FacebookAccountVerifiedAt { get; set; }
+
+    public DateTime LinkedInAccountVerifiedAt { get; set; }
+
+}
