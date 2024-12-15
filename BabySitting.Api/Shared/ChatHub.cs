d8b2ba69fd50715318568d80fcc3d0c7372c1ce7
LukaPavlovicGit <lukapavlovic032@gmail.com>
2024-12-15 01:21:23 +0100

    read message fun, renaming

--- BabySitting.Api/Shared/ChatHub.cs
+++ BabySitting.Api/Shared/ChatHub.cs
@@ -3,8 +3,13 @@ namespace BabySitting.Api.Shared;
 
 public class ChatHub : Hub
 {
-    public async Task SendMessage(string user, string message)
+    public async Task DirectMessage(string user, string message)
     {
         await Clients.Client(Context.ConnectionId).SendAsync("ReceiveMessage", user, message);
+    }
+
+    public async Task MarkMessageAsRead(string user, string messageId)
+    {
+        await Clients.User(user).SendAsync("MessageRead", messageId);
     }
 }
