9e316822adaf0e355d346dd2ea55ecb377828e2a
LukaPavlovicGit <lukapavlovic032@gmail.com>
2025-01-06 01:46:23 +0100

    working on login

---
+++ BabySitting.Api/Features/AccountLogin.cs
@@ -0,0 +1,73 @@
+using BabySitting.Api.Database;
+using BabySitting.Api.Domain.Entities;
+using BabySitting.Api.Infrastructure;
+using BabySitting.Api.Shared;
+using Carter;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using static BabySitting.Api.Features.Account.AccountLogin;
+
+namespace BabySitting.Api.Features.Account;
+
+public class AccountLogin
+{
+    internal sealed class AccountLoginRequest
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public bool RememberMe { get; set; } = false;
+    }
+
+    internal record AccountLoginResponse(string Token, bool IsAccountCompleted);
+    
+    internal sealed class Query(AccountLoginRequest request) : IRequest<Result<AccountLoginResponse>>
+    {
+        public string Email { get; set; } = request.Email;
+        public string Password { get; set; } = request.Password;
+        public bool RememberMe { get; set; } = request.RememberMe;
+    }
+
+    internal sealed class Handler(ApplicationDbContext dbContext, SignInManager<User> signInManager, TokenProvider tokenProvider) : IRequestHandler<Query, Result<AccountLoginResponse>>
+    {
+        private readonly ApplicationDbContext _dbContext = dbContext;
+        private readonly SignInManager<User> _signInManager = signInManager;
+        private readonly TokenProvider _tokenProvider = tokenProvider;
+
+        public async Task<Result<AccountLoginResponse>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var result = await _signInManager.PasswordSignInAsync(request.Email, request.Password, request.RememberMe, lockoutOnFailure: false);
+
+            if (!result.Succeeded)
+            {
+                throw new ApplicationException(result.ToString());
+            }
+            // TODO: Handle two-factor authentication case
+            if (result.RequiresTwoFactor)
+            {
+                throw new ApplicationException(result.ToString());// Handle two-factor authentication case
+            }
+            // TODO: Handle lockout scenario
+            if (result.IsLockedOut)
+            {
+                throw new ApplicationException(result.ToString());// Handle lockout scenario
+            }
+
+            var user = await _signInManager.UserManager.FindByEmailAsync(request.Email);
+            string token = _tokenProvider.Create(user!);
+            return new AccountLoginResponse(token, user!.IsAccountCompleted);
+        }
+    }
+}
+
+public class AccountLoginEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/api/account/login", async (AccountLoginRequest request, ISender sender) =>
+        {
+            var command = new AccountLogin.Query(request);
+            var result = await sender.Send(command);
+            return Results.Ok(result.Value);
+        });
+    }
+}
